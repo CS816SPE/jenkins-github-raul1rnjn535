e75b4fc5205ca8ea52367d6eadc78b9b5223cf97
raul1rnjn535_3 <ranjanraul10@gmail.com>
2019-01-28 12:23:36 +0530

    hello world included loop in code

--- helloworld.cpp
+++ helloworld.cpp
@@ -3,5 +3,9 @@ using namespace std;
 
 int main(){
 	cout<<"Hello World! The answer to Life, Universe and everything is 42..."<<endl;
+	int iter=42;
+	for(int i=0; i< iter; i++){
+		cout<<"Iteration..."<<i<<endl;
+	}
 	return 0;
 }
